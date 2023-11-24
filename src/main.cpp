a4d1f34cdfa0035f34db8ba2de1b2dd10b14ec8c
ptrckgaf <gafrik.patrik@gmail.com>
2023-11-24 23:24:52 +0100

    initial commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,39 @@
-//
-// Created by pgafrik on 11/23/23.
-//
+#include <simlib.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+
+
+Facility Laboratorium1("Laboratorium pre prijem mlieka");
+Facility Cistenie("Cistiaca Linka");
+Facility Normalizacia("Normalizacna Linka");
+Facility Pasterizacia("Pasterizacna Linka");
+Facility Chladenie("Chladiaca Linka");
+Facility Laboratorium2("Mikrobiologicke laboratorium");
+Facility Zrazanie("Zrazacia Linka");
+Facility Krajanie("Krajacia Linka");
+Facility Lisovanie("Lisovacia Linka");
+Facility Formovanie("Formovacia Linka");
+Facility Solenie("Soliaca Linka");
+
+
+
+
+
+
+
+
+
+
+class KontrolaKyslosti: public Process {
+
+    void Behavior() {
+
+        Seize(Laboratorium1);
+        Release(Laboratorium1);
+
+        (new KontrolaKyslosti)->Activate();
+    }
+
+};
+
